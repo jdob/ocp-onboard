9a7c912948ce37661d153146f64f574f03fd735d
Jay Dobies <jason.dobies@redhat.com>
2016-12-07 14:32:17 -0500

    Fixed copyright

--- conf.py
+++ conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Partner Onboarding Guide'
-copyright = u'2016, Jay Dobies'
+copyright = u'2016, Red Hat'
 author = u'Jay Dobies'
 
 # The version info for the project you're documenting, acts as replacement for
